08123377e087853da2bc57720dd4c35e11d610d0
EsbenNedergaard <33868248+EsbenNedergaard@users.noreply.github.com>
2017-11-21 15:02:34 +0100

    Update split_to_words.c

--- split_to_words.c
+++ split_to_words.c
@@ -6,6 +6,8 @@
 #define MAX_AMOUNT_WORDS 20
 
 int SplitToWords(char *inputText, char outputArray[][MAX_SIZE_WORD]);
+void Splitter(char outputArray[][MAX_SIZE_WORD], int *count, int *lenghtword);
+void TransferCharacter(char outputArray[][MAX_SIZE_WORD], int *count, int *lenghtword, char inputChar);
 
 int main() {
    char str[MAX_TEXT_LENGHT];
@@ -19,32 +21,43 @@ int main() {
    
    printf("\nOriginal String is: %s", str);
    printf("\nYour string is seperated into:\n");
-   printf("Value of count: %d\n", count);
+
    for(int i=0; i < count; i++)
       printf("%s\n", splitStrings[i]);
    
    return 0;
 }
 
+/* This is our overall function for splitting up a text into individual strings, 
+   which we need for when compareing the words with our database.
+
+   It takes takes an inputText string, as first parameter, and an two dimensional 
+   array which the indvidual strings are stored in*/
 int SplitToWords(char *inputText, char outputArray[][MAX_SIZE_WORD]) {
-   int i, j=0, count=0;
+   int i, j = 0, WordCount = 0;
 
    for(i=0; i<=(strlen(inputText)); i++) {    
       if (isalnum(inputText[i])) 
-         outputArray[count][j++] = inputText[i];
-      else if (inputText[i]==' '|| inputText[i]=='\0') {
-         outputArray[count][j] = '\0';
-         count++;
-         j=0;
-         }
+         TransferCharacter(outputArray, &WordCount, &j, inputText[i]);
+      else if (inputText[i] == ' '|| inputText[i] == '\0') {
+         Splitter(outputArray, &WordCount, &j);
+      }
       else {
-         outputArray[count][j] = '\0';
-         count++;
-         j=0;
-         outputArray[count][j] = inputText[i];
-         j++;
+         Splitter(outputArray, &WordCount, &j);
+         TransferCharacter(outputArray, &WordCount, &j, inputText[i]);
       }
     }
-   printf("Value of count in function: %d\n", count);
-   return count;
+   return WordCount;
+}
+
+/*Pretty sure there is a smarter way to transfer over the 2D array, than transfering it and it coordinates over in 3 bits*/
+void Splitter(char outputArray[][MAX_SIZE_WORD], int *WordCount, int *lenghtword) {
+   outputArray[*WordCount][*lenghtword] = '\0';
+   *lenghtword = 0;
+   (*WordCount)++;
+}
+
+void TransferCharacter(char outputArray[][MAX_SIZE_WORD], int *WordCount, int *lenghtword, char inputChar) {
+   outputArray[*WordCount][*lenghtword] = inputChar;
+   (*lenghtword)++;
 }
