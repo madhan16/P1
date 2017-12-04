d2e5238053edf221791e35814070298a4eb85462
Tobias Dybdahl <33865716+tdybda17@users.noreply.github.com>
2017-12-04 14:25:38 +0100

    Update from-string-to-struct.c

--- from-string-to-struct.c
+++ from-string-to-struct.c
@@ -8,7 +8,8 @@
 
 typedef struct word {
     char word[WORD_NAME_LGT];
-    int is_capital;
+    int  is_capital;
+    int  is_twittertag;
 } word_tbl;
 
 int in_array(char val, char *arr);
@@ -16,17 +17,17 @@ int split_to_words(char *str, word_tbl *wl);
 word_tbl transfer_word_to_list(word_tbl *wl, char *word);
 void print_word_list(word_tbl wl);
 int word_starts_upper(char *str);
+int is_twitter_tag(char *str);
 
 int main(void) {
     int i, countWords;
     word_tbl word_list[WORD_LIST_LGT];
-    char tweet[] = "Our boy @tobygreendj crushing it on @mintsyndicate rn! Big up @austinkramer";
+    char tweet[] = "Our boy @tobygreendj crushing it. $Tesla that's on @mintsyndicate rn! Big up @austinkramer SygtNokBro";
     
     countWords = split_to_words(tweet, word_list);
     
     for(i = 0; i < countWords; i++)
         print_word_list(word_list[i]);
-    
     return 0;
 }
 
@@ -35,11 +36,12 @@ int split_to_words(char *str, word_tbl *wl) {
     /*temperary string to store word*/
     char temp[30];
     /*special characters to check for*/
-    char specialSigns[10] = {'!', '?', '.', ',', '-', '_'};
+    char signsWithSpaceAfter[10] = {'!', '?', '.', ','};
+    char twitterTags[10] = {'#', '@', '$', '-', '\''};
     int countWords;
     
     for(i = 0; i <= (strlen(str)); i++) {
-        if (isalnum(str[i])) {
+        if (isalnum(str[i]) || in_array(str[i], twitterTags)) {
             temp[j++] = str[i];
         }
         else if(str[i] == ' ' || str[i] == '\0') {
@@ -50,19 +52,19 @@ int split_to_words(char *str, word_tbl *wl) {
             j = 0;
         }
         /*if sign is in array*/
-        else if(in_array(str[i], specialSigns)) {
+        else if(in_array(str[i], signsWithSpaceAfter)) {
             /*complete temperary string found before special sign*/
             temp[j] = '\0';
             wl[k++] = transfer_word_to_list(wl, temp);
             j = 0;
             /*add special sign*/
-            temp[j] = str[i]; temp[j + 1] = '\0';
-            wl[k++] = transfer_word_to_list(wl, temp);
-            j = 0;
-            /*skip space after special sign*/
-            i++;
+            temp[j++] = str[i];
         }
     }
+    
+    temp[j] = '\0';
+    wl[k++] = transfer_word_to_list(wl, temp);
+    
     countWords = k;
     return countWords;
 }
@@ -83,6 +85,7 @@ word_tbl transfer_word_to_list(word_tbl *wl, char *word) {
     word_tbl result;
     strcpy(result.word, word);
     result.is_capital = word_starts_upper(word);
+    result.is_twittertag = is_twitter_tag(word);
     
     return result;
 }
@@ -96,9 +99,22 @@ int word_starts_upper(char *str) {
         return 0;
 }
 
-void print_word_list(word_tbl wl) {
-    char *yes_or_no;
+int is_twitter_tag(char *str) {
+    int i;
+    char twitterTags[10] = {'#', '@', '$'};
     
-    yes_or_no = (wl.is_capital ? "yes" : "no");
-    printf("Word: %-17s Is capital: %s\n", wl.word, yes_or_no);
+    for(i = 0; i < 3; i++) {
+        if(twitterTags[i] == str[0])
+            return 1;
+    }
+    return 0;
+}
+
+void print_word_list(word_tbl wl) {
+    char *is_upper,
+         *is_twittertag;
+    
+    is_upper = (wl.is_capital ? "yes" : "no");
+    is_twittertag = (wl.is_twittertag ? "yes" : "no");
+    printf("Word: %-17s Capital: %-4s Twittertag: %s\n", wl.word, is_upper, is_twittertag);
 }
